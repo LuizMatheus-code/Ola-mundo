e20d1ee59b5c09e108fea9c2a31fbfbc0e0e28ce
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-07-27 11:05:13 -0300

    Update condicao.js

---
+++ Javascript/condicao.js
@@ -0,0 +1,7 @@
+var velocidade = 60
+console.log(`a velocidade do carro: ${velocidade}`)
+if (velocidade == 60) {
+    console.log("a velocidade igualou 60")
+} else {
+    console.log("a velocidade nao igualou 60")
+}
