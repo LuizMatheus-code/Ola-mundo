d384e2ecd20a51a821045aa170cadfcc35309482
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-05 08:59:33 -0300

    site

---
+++ HTML/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora, sao ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = "amanhecer.jpg"
+        document.body.style.background = "#e2cd9f"
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = "entardecer.jpg"
+        document.body.style.background = "rgb(179, 99, 99)"
+    } else {
+        //boa noite
+        img.src = "anoitecer.jpg"
+        document.body.style.background = "rgb(90, 99, 99)"
+    }
+}
+
